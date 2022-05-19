c1866ad2314be33186ef7bf688fd4c7945388413
Anselm Schüler <mail@anselmschueler.com>
2022-05-19 14:12:22 +0200

    Add tetris overlay that moves with nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,21 +2,26 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs?ref=945ec49";
   outputs = { nixpkgs, flake-utils, self }:
-    let ghc-version = "ghc8107";
+    let
+      ghc-version = "ghc8107";
+      mkTetris = hkgs: import ./default.nix { inherit ghc-version hkgs; };
     in flake-utils.lib.eachDefaultSystem (system: {
       defaultPackage = self.packages.${system}.default;
       # defaultPackage is deprecated as of Nix 2.7.0
       packages = rec {
         default = tetris;
-        tetris = import ./default.nix {
-          hkgs =
-            nixpkgs.legacyPackages.${system}.haskell.packages.${ghc-version};
-          inherit ghc-version;
-        };
+        tetris = mkTetris
+          nixpkgs.legacyPackages.${system}.haskell.packages.${ghc-version};
       };
     }) // {
-      overlays.default = pkgs: pkgs-super: {
-        tetris = self.packages.${pkgs.system}.default;
+      overlays = {
+        default = self.overlays.tetris-locked;
+        tetris-locked = final: prev: {
+          tetris = self.packages.${final.system}.default;
+        };
+        tetris = final: prev: {
+          tetris = mkTetris final.haskell.packages.${ghc-version};
+        };
       };
     };
 }
